45a975e1ca2af8c1549f74cef486b70b8d1babdb
ferty460 <rostovcevvladimir460@gmail.com>
2023-03-09 20:16:26 +0300

    buttons, commands, init

---
+++ telegram-bot/src/main/java/com/example/telegrambot/components/BotCommands.java
@@ -0,0 +1,16 @@
+package com.example.telegrambot.components;
+
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+
+import java.util.List;
+
+public interface BotCommands {
+    List<BotCommand> LIST_OF_COMMANDS = List.of(
+            new BotCommand("/start", "start bot"),
+            new BotCommand("/help", "bot info")
+    );
+
+    String HELP_TEXT = "This following commands are available to you:\n\n" +
+            "/start - start the bot\n" +
+            "/help - help menu";
+}
